ba872c912c5e0d92f64aea9edf219799519e2cad
pranavkumara <pranavkumara90@gmail.com>
2026-08-11 19:29:02 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -10,7 +10,7 @@ from app.config import settings
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host=settings.HOST,
-        port=settings.PORT,
+        host="0.0.0.0",
+        port=9000,
         reload=True
     )
